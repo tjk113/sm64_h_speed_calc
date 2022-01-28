5cd8e4224c744fe916de37662ec3475bc4fc3e03
tjk113 <94358965+tjk113@users.noreply.github.com>
2022-01-28 12:35:54 -0500

    made it actually work
    fixed the math (thanks to jong), and statically linked the c++ std library so that there shouldn't be any issues with running the exe on a windows machine on its own.

--- H_Speed_Calc.cpp
+++ H_Speed_Calc.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
+#include <iomanip>
 #include <stdlib.h>
 
-float getSpd(float distPerFrame, int n) {
-    return 4*(distPerFrame + 48) + 4*n;
+float getSpd(float yDist, int n) {
+    return 4*(yDist / n) + 8*n - 216; // jongyon's hspd equation :D
 }
 
 int main() {
 
-    float marioY, starY, distPerFrame; // mario and star y-positions, y-distance
+    float marioY, starY, yDist, yDistPerFrame; // mario and star y-positions, y-distance
     int n; // number of frames
     char restart;
     
     std::cout << "\nMario Y, Star Y, Number of Frames: "; std::cin >> marioY >> starY >> n;
-
-    distPerFrame = (starY - marioY) / n;
     
-    std::cout << '\n' << "Required H Speed = " << getSpd(distPerFrame, n);
-    std::cout << '\n' << "Required Y Speed = " << distPerFrame << std::endl;
+    starY -= 19.99; // optimal distance below star
+    marioY += 160; // mario hitbox height
+    yDist = starY - marioY;
+    yDistPerFrame = (getSpd(yDist, n) / 4) + 52;
+    
+    std::cout << std::setprecision(10) << '\n' << "Required H Speed = " << getSpd(yDist, n);
+    std::cout << std::setprecision(10) << '\n' << "Required Y Speed = " << yDistPerFrame << std::endl;
     std::cout << '\n' << "New Calculation? [y/n] "; std::cin >> restart;
 
-    if( restart == std::tolower('Y') ) {
+    if( std::tolower(restart) == 'y' ) {
         main();
     } else {
         return 0;
